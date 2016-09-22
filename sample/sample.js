b2b13c5c0064e7fecd71fbddbe7160c185023d72
jiheejeong <jiheejeong93@gmail.com>
2016-09-22 13:45:30 +0900

    commit

---
+++ sample/sample.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var passport = require('passport');
+var session = require('express-session');
+var NaverTokenStrategy = require('../lib/index.js').Strategy;
+
+var client_id = '************ your app client id ************';
+var client_secret = '************ your app client secret ************';
+var callback_url = '************ your app callback url ************';
+
+passport.serializeUser(function(user, done) {
+	done(null, user);
+});
+
+passport.deserializeUser(function(obj, done) {
+	done(null, obj);
+});
+
+passport.use(new NaverTokenStrategy({
+    clientID: client_id,
+    clientSecret: client_secret,
+    callbackURL: callback_url
+}, function(accessToken, refreshToken, profile, done) {
+		user = {
+			provider: 'naver',
+			id : profile.id,
+			name: profile.name,
+			email: profile.email,
+			nickName : profile.nickName,
+			enc_id : profile.enc_id,
+			profile_image : profile.profile_image,
+			age : profile.age,
+			birthday : profile.birthday,
+			naver: profile._json
+		};
+		return done(null, user);
+}));
+
+var app = express();
+app.use(passport.initialize());
+
+app.get("/auth/naver/token", passport.authenticate('naver-token', null), function(req, res, next){
+	if (err) return next(err);
+
+	if (req.user) {
+		req.send('success');
+	} else {
+		req.send('fail');
+	}
+});
+
+app.listen(3000);
+
